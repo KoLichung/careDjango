18321566cfbb36e9fe173275c21053dcf4075663
JasonKo <jason@kosbrother.com>
2022-06-09 13:53:41 +0800

    add host

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,7 +24,11 @@ SECRET_KEY = 'django-insecure-^pxr2pc@d81=s1*hh9t_pcs4^a-pazd_+@lv40nscf7-oh$&4w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "202.182.105.11",
+    '127.0.0.1',
+    'localhost',
+]
 
 ADMINS = (
     ('user0', '0000000000'),
