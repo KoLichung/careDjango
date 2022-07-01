66327504ce2f4a5537c9c053463048df452171d3
JasonKo <jason@kosbrother.com>
2022-07-01 15:39:53 +0800

    add city county

--- app/modelCore/admin.py
+++ app/modelCore/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import  User
+from .models import  User, City, County
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'phone')
 
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(County)
+class CountyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'city', 'name')
 
 
