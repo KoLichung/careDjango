2addf525a1d0b743f950ce33e9e8f46c1b508c8e
JasonKo <jason@kosbrother.com>
2022-07-22 16:30:42 +0800

    add module

---
+++ app/newebpayApi/MPG_Gateway.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# MPG 多元付款交易
+
+import time
+import module
+import urllib.parse
+import webbrowser
+
+
+# 設定api網址、建立商店代號.key.iv
+api_url = "https://ccore.newebpay.com/MPG/mpg_gateway"
+merchant_id = "TEK1656560395"
+key = "UbeH372MUkjIwmHEpHDvlULoJrv0HeBZ"
+iv = "CGBifDtHUOAqoYwP"
+
+
+# 建立MPG交易時要填入的資料、設定版本號(以信用卡一次付清為例)
+test_data = {
+    "MerchantID": merchant_id,
+    "Version": "2.0",
+    "TimeStamp": int(time.time()),
+    "MerchantOrderNo": f"No{int(time.time())}",
+    "Amt": 40,
+    "CREDIT": 1,
+    "ItemDesc": "test",
+    "LoginType": "0",
+    "Email": "",
+    "RespondType": "JSON",
+}
+
+
+# 將資料編成搜尋字串，並依照不同加密模式進行加密及SHA256壓碼
+query_str = urllib.parse.urlencode(test_data)
+encrypt_data = module.aes256_cbc_encrypt(query_str, key, iv)
+hashs = module.sha256_hash(encrypt_data, key, iv)
+
+
+# 建立POST參數並帶入MPG
+params = {
+    "MerchantID": merchant_id,
+    "TradeInfo": encrypt_data,
+    "TradeSha": hashs,
+    "Version": test_data["Version"],
+}
+
+
+# 建立測試用的web頁面，將POST參數代入
+with open("MPG.html", 'w', encoding="utf-8") as f:
+    html_string = f"<!DOCTYPE html><head><meta charset='utf-8'><title>MPG</title></head><body><form name='Newebpay' method='post' action={api_url}>測試URL: {api_url}<p>MerchantID:<input type='text' name='MerchantID' value={params['MerchantID']}><br><br>TradeInfo:<input type='text' name='TradeInfo' value={params['TradeInfo']}><br><br>TradeSha:<input type='text' name='TradeSha' value={params['TradeSha']}><br><br>Version:<input type='text' name='Version' value={params['Version']}><br><br><input type='submit' value='Submit'></form></body></html>"
+    f.write(html_string)
+webbrowser.open("MPG.html", "r")
---
+++ app/newebpayApi/module.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import hashlib
+import binascii
+from Crypto.Cipher import AES
+from Crypto.Cipher.AES import MODE_GCM, MODE_CBC
+from Crypto.Util.Padding import pad, unpad
+
+
+# AES256-CBC加密 (parameter=dic型態的參數, key=商店HashKey, iv=商店HashIV)
+def aes256_cbc_encrypt(parameter, key, iv):
+    parameter, key, iv = parameter.encode(), key.encode(), iv.encode()
+    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+    encrypt_data = str(binascii.b2a_hex(cipher.encrypt(
+        pad(parameter, block_size=32))), "utf-8")
+    return encrypt_data
+
+
+# AES256-CBC解密 (encrypt_data=交易完成系統回傳之參數, key=商店HashKey, iv=商店HashIV)
+def aes256_cbc_decrypt(encrypt_data, key, iv):
+    encrypt_data, key, iv = encrypt_data.encode(), key.encode(), iv.encode()
+    cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+    parameter = unpad(cipher.decrypt(binascii.a2b_hex(
+        encrypt_data)), block_size=32).decode("utf-8")
+    return parameter
+
+
+# SHA256壓碼 (TradeInfo=AES256加密後的值, key=商店HashKey, iv=商店HashIV)
+def sha256_hash(TradeInfo, key, iv):
+    TradeValue = "HashKey=" + key + "&" + TradeInfo + "&" + "HashIV=" + iv
+    hashs = hashlib.sha256(TradeValue.encode("utf-8")).hexdigest()
+    hashs = str.upper(hashs)
+    return hashs
