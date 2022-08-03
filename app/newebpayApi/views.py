e72c4cdbdfc12262e6518ca9a687fabf138e8f13
ScottFang <fangshengjie@fangshengjiedeMacBook-Pro.local>
2022-08-03 17:57:01 +0800

    test 80 port

--- app/newebpayApi/views.py
+++ app/newebpayApi/views.py
@@ -133,7 +133,7 @@ class MpgTrade(APIView):
             "MerchantOrderNo":"202208030007",
             "Amt": 2000,
             "ItemDesc": "test",       
-            "NotifyURL": "http://localhost:80/newebpayApi/notifyurl_callback"
+            "NotifyURL": "http://127.0.0.1:80/newebpayApi/notifyurl_callback"
         }
 
         query_str = urllib.parse.urlencode(data)
