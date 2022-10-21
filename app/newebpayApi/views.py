4125ea75b0c0a10d348c7fdcfa1726e041acfda1
JasonKo <jason@kosbrother.com>
2022-10-21 09:59:45 +0800

    change niginx

--- app/newebpayApi/views.py
+++ app/newebpayApi/views.py
@@ -75,7 +75,7 @@ class CreateMerchant(APIView):
                 "ManagerEmail": "jason@kosbrother.com",
                 "DisputeMail": "jason@kosbrother.com",
                 "MerchantEmail": "jason@kosbrother.com",
-                "MerchantID": "ACE55"+str(user.id),
+                "MerchantID": "ACE66"+str(user.id),
                 "MCType": 1,
                 "MerchantName": "杏心合作商店"+str(user.id),
                 "MerchantNameE": "XinShing"+str(user.id),
