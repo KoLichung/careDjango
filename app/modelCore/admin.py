d2ffbd1cc922fbb9426c37d2afc62edabae955b1
JasonKo <jason@kosbrother.com>
2022-12-29 10:30:07 +0800

    fix

--- app/modelCore/admin.py
+++ app/modelCore/admin.py
@@ -111,7 +111,7 @@ class ChatroomUserShipAdmin(admin.ModelAdmin):
 
 @admin.register(ChatroomMessage)
 class ChatroomMessageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'chatroom','user', 'case')
+    list_display = ('id', 'chatroom','user', 'case', 'order')
 
 @admin.register(SystemMessage)
 class SystemMessageAdmin(admin.ModelAdmin):
