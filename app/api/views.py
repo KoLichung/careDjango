0a3ab0ede40c94a5e0e98e57a514e3fa97b51d1d
JasonKo <jason@kosbrother.com>
2023-01-05 13:34:58 +0800

    fix

--- app/api/views.py
+++ app/api/views.py
@@ -990,7 +990,7 @@ class CreateCase(APIView):
                 order.base_money = order.work_hours * wage
 
                 # need to change in the future
-                order.platform_percent = platform_percent_cal(user,order)
+                order.platform_percent = platform_percent_cal(servant,order)
                 order.newebpay_percent = get_newebpay_percent()
                 order.save()
                 Review.objects.create(order=order,case=order.case,servant=order.servant)
@@ -1257,7 +1257,7 @@ class CreateServantOrder(APIView):
         order.base_money = order.work_hours * wage
 
         # need to change in the future
-        order.platform_percent = platform_percent_cal(user,order)
+        order.platform_percent = platform_percent_cal(servant,order)
         order.newebpay_percent = get_newebpay_percent()
         order.save()
         Review.objects.create(order=order,case=order.case,servant=order.servant)
@@ -1428,7 +1428,7 @@ class ApplyCase(APIView):
             order.wage_hour =wage
             order.base_money = order.work_hours * wage
 
-            order.platform_percent = platform_percent_cal(case.user,order)
+            order.platform_percent = platform_percent_cal(servant,order)
             order.newebpay_percent = get_newebpay_percent()
             order.save()
             Review.objects.create(order=order,case=order.case,servant=order.servant)
@@ -2077,7 +2077,7 @@ class EditCase(APIView):
                 order.base_money = order.work_hours * wage
 
                 # need to change in the future
-                order.platform_percent = platform_percent_cal(user,order)
+                order.platform_percent = platform_percent_cal(servant,order)
                 order.newebpay_percent = get_newebpay_percent()
                 order.save()
                 Review.objects.create(order=order,case=order.case,servant=order.servant)
@@ -2255,7 +2255,7 @@ def continuous_time_cal(order):
 
     return total_hours
 
-def platform_percent_cal(user,order):
+def platform_percent_cal(servant,order):
     orders = Order.objects.all()
     today = datetime.datetime.today()
     current_year = today.year
@@ -2263,8 +2263,8 @@ def platform_percent_cal(user,order):
     # base_percent = 2.8
     work_hours = order.work_hours
     orders_total_hours = work_hours 
-    if orders.filter(user=user,start_datetime__year=current_year,start_datetime__month=current_month,state='paid').count() != 0:
-        accumulate_work_hours = orders.filter(user=user,start_datetime__year=current_year,start_datetime__month=current_month,state='paid').aggregate(Sum('work_hours'))['work_hours__sum']
+    if orders.filter(servant=servant,start_datetime__year=current_year,start_datetime__month=current_month,state='paid').count() != 0:
+        accumulate_work_hours = orders.filter(servant=servant,start_datetime__year=current_year,start_datetime__month=current_month,state='paid').aggregate(Sum('work_hours'))['work_hours__sum']
         print('accumulate_work_hours',accumulate_work_hours)
         orders_total_hours += accumulate_work_hours
     
