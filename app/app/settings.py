ec295ffc50b9d2300a3ec403cb3095460044a5f9
JasonKo <jason@kosbrother.com>
2023-04-25 15:45:56 +0800

    fix

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,7 +31,6 @@ SMS_PASSWORD=env('SMS_PASSWORD')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
